9c0610535abb23bca871df8c66abf02bd4a434de
Steven Schulte <shotgunsteven4@yahoo.com>
2015-09-02 10:43:10 -0600

    Checkpoint 22 completed, binary search

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,5 +1,5 @@
-require_relative 'entry'
-require "csv"
+require_relative 'entry' # relative custom Entry class
+require "csv" # import CSV object to parse CSV files
 
 class AddressBook
     attr_accessor :entries
@@ -8,6 +8,7 @@ class AddressBook
       @entries = []
     end
 
+    # Add individual entries
     def add_entry(name, phone_number, email)
      index = 0
      @entries.each do |entry|
@@ -19,6 +20,7 @@ class AddressBook
      @entries.insert(index, Entry.new(name, phone_number, email))
     end
 
+    # Import from CSV files
     def import_from_csv(file_name)
        csv_text = File.read(file_name)
        csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
@@ -28,5 +30,25 @@ class AddressBook
          add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
        end
     end
-    
+
+    # Search AddressBook for a specific entry by name
+    def binary_search(name)
+      lower = 0
+      upper = entries.length - 1
+
+      while lower <= upper
+        mid = (lower + upper) / 2
+        mid_name = entries[mid].name
+
+        if name == mid_name
+          return entries[mid]
+        elsif name < mid_name
+          upper = mid - 1
+        elsif name > mid_name
+          lower = mid + 1
+        end
+      end
+
+     return nil
+    end
 end
